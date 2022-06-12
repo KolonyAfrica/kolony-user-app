47367a45da953e9fbc159e34be08e753d4237e25
darausi <daniel.arausi@eccenca.com>
2022-06-12 20:28:06 +0100

    fixed landmark animation screen

--- screens/LandMark/set-landmark.screens.tsx
+++ screens/LandMark/set-landmark.screens.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {TouchableOpacity} from 'react-native';
+import {Animated, TouchableOpacity} from 'react-native';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useTheme} from 'styled-components/native';
 import {
@@ -14,6 +14,7 @@ import {
   BUTTON_TYPES,
   Icon,
 } from '../../components/shared';
+import {SCREEN_WIDTH} from '../../components/shared/common/constants';
 import {
   HorizontalWrapper,
   ScreenWrapper,
@@ -28,6 +29,9 @@ const SetLandMark = () => {
   const [selectedLandmarks, setSelectedLandmarks] = React.useState<string[]>(
     [],
   );
+  const landmarkBoxAnimatedValue = React.useRef(new Animated.Value(0)).current;
+  const [landmarkIndexToDelete, setLandmarkDeleteIndex] =
+    React.useState<number>();
 
   const handleLandmarkSelection = React.useCallback((landmark: string) => {
     setSelectedLandmarks(landmarks =>
@@ -35,14 +39,68 @@ const SetLandMark = () => {
     );
   }, []);
 
-  const handleRemoveLandMark = React.useCallback(index => {
-    return () => {
-      setSelectedLandmarks(landmarks => [
-        ...landmarks.slice(0, index),
-        ...landmarks.slice(index + 1),
-      ]);
-    };
-  }, []);
+  React.useEffect(() => {
+    if (landmarkIndexToDelete !== undefined) {
+      const index = landmarkIndexToDelete;
+      Animated.spring(landmarkBoxAnimatedValue, {
+        toValue: 1,
+        friction: 10,
+        useNativeDriver: true,
+      }).start(() => {
+        setSelectedLandmarks(landmarks => [
+          ...landmarks.slice(0, index),
+          ...landmarks.slice(index + 1),
+        ]);
+        setLandmarkDeleteIndex(undefined);
+        Animated.spring(landmarkBoxAnimatedValue, {
+          toValue: 0,
+          useNativeDriver: true,
+        }).start();
+      });
+    }
+  }, [landmarkBoxAnimatedValue, landmarkIndexToDelete]);
+
+  const landmarkTransformX = landmarkBoxAnimatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, -SCREEN_WIDTH],
+  });
+
+  const landmarkTransformY = landmarkBoxAnimatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, -66],
+  });
+
+  const deletedLandmarkStyle = {
+    transform: [
+      {
+        translateX: landmarkTransformX,
+      },
+      {
+        translateY: 0,
+      },
+    ],
+  };
+  const nonDeletedLandmarkStyle = {
+    transform: [
+      {
+        translateY: landmarkTransformY,
+      },
+      {
+        translateX: 0,
+      },
+    ],
+  };
+
+  const precinctStyle = {
+    transform: [
+      {
+        translateY: 0,
+      },
+      {
+        translateX: 0,
+      },
+    ],
+  };
 
   const theme = useTheme();
   return (
@@ -63,40 +121,47 @@ const SetLandMark = () => {
           <Spacing direction="vertical" size={MARGIN_SIZES.small} />
 
           {selectedLandmarks.map((landmark, index) => (
-            <VerticalWrapper
-              marginBottom={16}
+            <Animated.View
               key={landmark + index}
-              fill
-              align="flex-start">
-              <StyledText
-                fontSize={theme.fontSizes.small}
-                color={theme.palette.tertiary.grey310}
-                fontWeight={400}
-                marginBottom={theme.margin.small}>
-                Landmark {index + 1}
-              </StyledText>
-              <HorizontalWrapper fill justify="space-between">
-                <LandmarkBox>
-                  <Icon
-                    name={ICON_NAME.flag}
-                    color={theme.palette.tertiary.grey210}
-                  />
-                  <StyledText
-                    fontSize={theme.fontSizes.small}
-                    color={theme.palette.tertiary.grey320}
-                    fontWeight={400}
-                    marginLeft={theme.margin.small}>
-                    {landmark}
-                  </StyledText>
-                </LandmarkBox>
-                <TouchableOpacity onPress={handleRemoveLandMark(index)}>
-                  <Icon name={ICON_NAME.add} direction="right" />
-                </TouchableOpacity>
-              </HorizontalWrapper>
-            </VerticalWrapper>
+              style={
+                landmarkIndexToDelete === index
+                  ? deletedLandmarkStyle
+                  : index > landmarkIndexToDelete!
+                  ? nonDeletedLandmarkStyle
+                  : precinctStyle
+              }>
+              <VerticalWrapper marginBottom={16} fill align="flex-start">
+                <StyledText
+                  fontSize={theme.fontSizes.small}
+                  color={theme.palette.tertiary.grey310}
+                  fontWeight={400}
+                  marginBottom={theme.margin.small}>
+                  Landmark {index + 1}
+                </StyledText>
+                <HorizontalWrapper fill justify="space-between">
+                  <LandmarkBox>
+                    <Icon
+                      name={ICON_NAME.flag}
+                      color={theme.palette.tertiary.grey210}
+                    />
+                    <StyledText
+                      fontSize={theme.fontSizes.small}
+                      color={theme.palette.tertiary.grey320}
+                      fontWeight={400}
+                      marginLeft={theme.margin.small}>
+                      {landmark}
+                    </StyledText>
+                  </LandmarkBox>
+                  <TouchableOpacity
+                    onPress={() => setLandmarkDeleteIndex(index)}>
+                    <Icon name={ICON_NAME.add} direction="right" />
+                  </TouchableOpacity>
+                </HorizontalWrapper>
+              </VerticalWrapper>
+            </Animated.View>
           ))}
           {selectedLandmarks.length < 3 ? (
-            <>
+            <Animated.View>
               <LocationInput
                 label="Landmark"
                 placeholder="Alara Jibowu Yaba"
@@ -129,7 +194,7 @@ const SetLandMark = () => {
               </VerticalWrapper>
               <Spacing direction="vertical" size={MARGIN_SIZES.medium} />
               <Button text="Continue" type={BUTTON_TYPES.primary} fill />
-            </>
+            </Animated.View>
           ) : null}
         </StyledScrollView>
       </SafeAreaView>
